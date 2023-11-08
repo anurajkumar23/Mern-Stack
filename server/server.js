a651edb5281cdc71c97d943ed9c6574c4fce4fba
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-11-08 19:24:44 +0530

    register express

--- server/server.js
+++ server/server.js
@@ -5,5 +5,9 @@ app.get("/", (req, res) =>{
     res.status(200).send("Welcome to my server of Anuraj kumar Mernstack website")
 })
 
+app.get("/register" , (req,res) => {
+    res.status(200).send("Welcome to register Page")
+})
+
 const PORT = 8000;
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
